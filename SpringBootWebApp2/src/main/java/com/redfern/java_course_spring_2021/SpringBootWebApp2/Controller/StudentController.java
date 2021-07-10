e7708e6455821af3b6546a0c926f6081ab8076a0
jmredfern-ar <jmredfern1970@gmail.com>
2021-07-10 15:00:10 -0500

    Prior to nesting *ngFor

--- SpringBootWebApp2/src/main/java/com/redfern/java_course_spring_2021/SpringBootWebApp2/Controller/StudentController.java
+++ SpringBootWebApp2/src/main/java/com/redfern/java_course_spring_2021/SpringBootWebApp2/Controller/StudentController.java
@@ -3,14 +3,20 @@ package com.redfern.java_course_spring_2021.SpringBootWebApp2.Controller;
 import java.time.LocalDate;
 import java.time.Month;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -42,14 +48,47 @@ public class StudentController {
 		return studentService.save(student);
 	}
 	
-//	public String getStudents(Model model) {
-//		List<Student> students = studentService.findAll();
-//		List<Course> courses = courseService.findAll();
-//		System.out.println(Arrays.toString(students.toArray()));
-//		System.out.println(Arrays.toString(courses.toArray()));
-//		model.addAttribute("students", students);
-//		model.addAttribute("courses", courses);
-//		return "student-list.html";
-//	}
+	@GetMapping("/students/{id}")
+	public ResponseEntity<Student> getStudentById(@PathVariable Long id) {
+		Student student = studentService.findById(id);
+		return ResponseEntity.ok(student);
+	}
 	
+	// PUT REQUEST IS USED TO MODIFY AN EXISTING RECORD
+	@PutMapping("/students/{id}")
+	public ResponseEntity<Student> updateStudent(@PathVariable Long id, @RequestBody Student studentDetails) {
+		
+		// STEP ONE: RETRIEVE STUDENT RECORD FROM THE DATABASE USING ID
+		Student student = studentService.findById(id);
+		
+		// STEP TWO: UPDATE RETRIEVED STUDENT WITH NEW STUDENT DETAILS FROM FRONT END
+		student.setName(studentDetails.getName());
+		student.setEmail(studentDetails.getEmail());
+		student.setDob(studentDetails.getDob());
+		student.setAge(studentDetails.getAge());
+		
+		// STEP THREE: SAVE THE MODIFIED STUDENT RECORDD TO THE DB
+		Student updatedStudent = studentService.save(student);
+		
+		// RETURN A RESPONSE CONTAINING OK AND THE UPDATED STUDENT RECORD
+		return ResponseEntity.ok(updatedStudent);
+	}
+	
+	// DELETE REQUEST IS USED TO DELETE AN EXISTING STUDENT RECORD
+	@DeleteMapping("/students/{id}")
+	public ResponseEntity<Map<String, Boolean>> deleteStudent(@PathVariable Long id) {
+		
+		// STEP ONE: RETRIEVE STUDENT RECORD FROM THE DATABASE USING ID
+		Student student = studentService.findById(id);
+		
+		// STEP TWO: USE CALL SERVICE'S DELETE METHOD
+		studentService.delete(student);
+		
+		// STEP THREE: CREATE A HASHMAP TO HOLD A MSG (KEY) AND TRUE (VALUE)
+		Map<String, Boolean> response = new HashMap<String, Boolean>();
+		response.put("deleted", Boolean.TRUE);
+		
+		// RETURN MAP INSIDE RESPONSE ENTITY
+		return ResponseEntity.ok(response);
+	}
 }
--- SpringBootWebApp2/src/main/java/com/redfern/java_course_spring_2021/SpringBootWebApp2/Service/StudentService.java
+++ SpringBootWebApp2/src/main/java/com/redfern/java_course_spring_2021/SpringBootWebApp2/Service/StudentService.java
@@ -1,11 +1,13 @@
 package com.redfern.java_course_spring_2021.SpringBootWebApp2.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.redfern.java_course_spring_2021.SpringBootWebApp2.DAO.StudentRepo;
+import com.redfern.java_course_spring_2021.SpringBootWebApp2.Exception.ResourceNotFoundException;
 import com.redfern.java_course_spring_2021.SpringBootWebApp2.Model.Student;
 
 @Service
@@ -21,6 +23,15 @@ public class StudentService {
 
 	public Student save(Student student) {
 		return studentRepo.saveAndFlush(student);
+	}
+
+	public Student findById(Long id) {
+		return studentRepo.findById(id)
+				.orElseThrow(() -> new ResourceNotFoundException("Student not found with id: " + id));
+	}
+
+	public void delete(Student student) {
+		studentRepo.delete(student);
 	}
 
 }
