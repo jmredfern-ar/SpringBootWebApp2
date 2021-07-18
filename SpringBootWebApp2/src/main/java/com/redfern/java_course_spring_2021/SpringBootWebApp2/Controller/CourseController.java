641ce9dc7dad66d6fd60c25836dde835a9d816ac
jmredfern-ar <jmredfern1970@gmail.com>
2021-07-18 09:28:03 -0500

    Before adding Many To Many relationship to Angular FE.

--- SpringBootWebApp2/src/main/java/com/redfern/java_course_spring_2021/SpringBootWebApp2/Controller/CourseController.java
+++ SpringBootWebApp2/src/main/java/com/redfern/java_course_spring_2021/SpringBootWebApp2/Controller/CourseController.java
@@ -3,12 +3,10 @@ package com.redfern.java_course_spring_2021.SpringBootWebApp2.Controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.redfern.java_course_spring_2021.SpringBootWebApp2.DAO.CourseDAO;
 import com.redfern.java_course_spring_2021.SpringBootWebApp2.Model.Course;
 import com.redfern.java_course_spring_2021.SpringBootWebApp2.Service.CourseService;
 
@@ -17,28 +15,11 @@ import com.redfern.java_course_spring_2021.SpringBootWebApp2.Service.CourseServi
 @RequestMapping("/api/v1")
 public class CourseController {
 
-//	@Autowired
-//	CourseDAO courseDAO;
-
 	@Autowired
 	private CourseService courseService;
 	
 	@RequestMapping("/courses")
 	public List<Course> course() {
 		return courseService.findAll();
-	}
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	@RequestMapping("/addCourse")
-	public String addCourse(Course course) {
-		courseDAO.save(course);
-		return "course.jsp";
 	}
 }
