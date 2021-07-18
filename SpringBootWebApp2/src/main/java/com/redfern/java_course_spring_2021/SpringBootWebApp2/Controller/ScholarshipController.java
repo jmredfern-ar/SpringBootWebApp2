3b37836145963275887550efda1045ca0e2aaf86
jmredfern-ar <jmredfern1970@gmail.com>
2021-07-18 11:51:42 -0500

    Added @ManyToMany relationship

--- SpringBootWebApp2/src/main/java/com/redfern/java_course_spring_2021/SpringBootWebApp2/Controller/ScholarshipController.java
+++ SpringBootWebApp2/src/main/java/com/redfern/java_course_spring_2021/SpringBootWebApp2/Controller/ScholarshipController.java
@@ -5,25 +5,32 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.redfern.java_course_spring_2021.SpringBootWebApp2.Model.Scholarship;
 import com.redfern.java_course_spring_2021.SpringBootWebApp2.Service.ScholarshipService;
 
-@Controller
+@CrossOrigin(origins="http://localhost:4200")
+@RestController
 @RequestMapping("api/v1")
 public class ScholarshipController {
 	
 	@Autowired
 	ScholarshipService scholarshipService;
 	
-	@GetMapping("scholarships")
-	public String listScholarships(Model model) {
-		List<Scholarship> scholarships = scholarshipService.findAll();
-		model.addAttribute("scholarships", scholarships);
-		return "scholarship-list.html";
-		
-	}
+//	@GetMapping("scholarships")
+//	public String listScholarships(Model model) {
+//		List<Scholarship> scholarships = scholarshipService.findAll();
+//		model.addAttribute("scholarships", scholarships);
+//		return "scholarship-list.html";
+//		
+//	}
 
+	@GetMapping("/scholarships")
+	public List<Scholarship> getScholarships() {
+		return scholarshipService.findAll();
+	}
 }
