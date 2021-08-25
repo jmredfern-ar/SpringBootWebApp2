281bb2cccf34143d736d5321ba418270fd6b49e8
jmredfern-ar <jmredfern1970@gmail.com>
2021-08-25 17:58:05 -0500

    Final Copy

--- SpringBootWebApp2/src/main/java/com/redfern/java_course_spring_2021/SpringBootWebApp2/SpringBootWebApp2Application.java
+++ SpringBootWebApp2/src/main/java/com/redfern/java_course_spring_2021/SpringBootWebApp2/SpringBootWebApp2Application.java
@@ -4,6 +4,7 @@ package com.redfern.java_course_spring_2021.SpringBootWebApp2;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//MAIN APP
 @SpringBootApplication
 public class SpringBootWebApp2Application {
 
