aea9a7d67a6da7557d33d513cf207b5d3d6cc5f2
jmredfern-ar <jmredfern1970@gmail.com>
2021-08-26 09:03:19 -0500

    Third commit attempt - now w/ token

--- SpringBootWebApp2/src/main/java/com/redfern/java_course_spring_2021/SpringBootWebApp2/SpringBootWebApp2Application.java
+++ SpringBootWebApp2/src/main/java/com/redfern/java_course_spring_2021/SpringBootWebApp2/SpringBootWebApp2Application.java
@@ -4,7 +4,7 @@ package com.redfern.java_course_spring_2021.SpringBootWebApp2;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-//MAIN APP 2
+//MAIN APP 3rd ATTEMPT TO COMMIT
 @SpringBootApplication
 public class SpringBootWebApp2Application {
 
